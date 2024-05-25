fa9c13a82d29ff85390d68dc2b8bb3b95b220087
andrey <feand20041102@gmail.com>
2024-05-25 17:40:20 +0300

    up

--- my-app/src/app/app.routes.ts
+++ my-app/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
+import { EducationComponent } from './pages/education/education.component';
 
 export const routes: Routes = [
   {path: '', component: MainComponent},
+  {path: 'education', component: EducationComponent},
   {path: '**', redirectTo: ''}
 ];
